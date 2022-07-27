f1407e3f85fb72030592b0839b6d334ba1a8e695
zhiliaev <Zhilyaev.Dmitriy@gmail.com>
2022-07-28 00:37:34 +0300

    getopt docs

--- getopt/solution.c
+++ getopt/solution.c
@@ -29,7 +29,7 @@ int main (int argc, char* argv[])
       switch (next_option){
       case 'q':
           query = optarg;
-          // printf("q=%c\n", query);
+          // printf("q=%s\n", query);
       case 'i':
       case 'v':
       case -1:
